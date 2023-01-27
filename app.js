29126fcf7d7999ecd26fcc7711c532e2eee621b3
Daniel <dberhe490@gmail.com>
2023-01-27 13:06:27 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,57 @@
+//Budget Controller
+var budgetContoller = (function(){
+   //some code
+})(); 
+
+
+
+//UI controller
+var UIcontroller = (function(){
+    var DOMStirngs = {
+        inputType:'.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputBtn: '.add__btn'
+    }
+    return {
+        getInput: function(){
+           return {
+            type: document.querySelector(DOMStirngs.inputType).value, // wheather the inc or exp
+            description: document.querySelector(DOMStirngs.inputDescription).value,
+            value: document.querySelector(DOMStirngs.inputValue).value} 
+        },
+
+        getDOMStrings: function(){
+            return DOMStirngs; 
+        }
+    }
+
+
+})();
+
+
+//Global App Controller
+var controller = (function(budgetCtrl, UIctrl){
+    var DOM = UIctrl.getDOMStrings();
+    var ctrlAddItem = function(){
+        //1. Get the field input data
+        var input = UIctrl.getInput();
+        console.log(input)
+        //2. Add the Item to the budgetCottroller
+
+        //3. Add the item to the UI
+
+        //4. Calculate the budget
+
+        //5. Dispaly the budget on the UI
+
+    }
+    document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem)
+    document.addEventListener('keypress', function(event){
+        if(event.keyCode === 13 || event.which === 13){
+            ctrlAddItem();
+        }
+    })
+
+
+})(budgetContoller, UIcontroller);
